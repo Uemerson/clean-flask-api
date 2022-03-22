7e2a661bb24d4c866df6b1c91e6f8460ac1a09bc
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-03-21 23:16:53 -0300

    refactor: add customized error for missing param

--- src/presentation/controllers/signup_controller.py
+++ src/presentation/controllers/signup_controller.py
@@ -1,6 +1,12 @@
+from src.presentation.errors.missing_param_error import MissingParamError
+
+
 class SignUpController:
     def handle(self, http_request):
         if "name" not in http_request["body"]:
-            return {"statusCode": 400, "body": Exception("Missing param: name")}
+            return {"statusCode": 400, "body": MissingParamError("name")}
         if "email" not in http_request["body"]:
-            return {"statusCode": 400, "body": Exception("Missing param: email")}
+            return {
+                "statusCode": 400,
+                "body": MissingParamError("email"),
+            }
--- tests/presentation/controllers/test_signup_controller.py
+++ tests/presentation/controllers/test_signup_controller.py
@@ -1,4 +1,5 @@
 from src.presentation.controllers.signup_controller import SignUpController
+from src.presentation.errors.missing_param_error import MissingParamError
 
 
 def test_should_return_400_if_no_name_is_provided():
@@ -12,7 +13,7 @@ def test_should_return_400_if_no_name_is_provided():
     }
     http_response = sut.handle(http_request)
     assert http_response["statusCode"] == 400
-    assert http_response["body"].args == Exception("Missing param: name").args
+    assert http_response["body"].args == MissingParamError("name").args
 
 
 def test_should_return_400_if_no_email_is_provided():
@@ -26,4 +27,4 @@ def test_should_return_400_if_no_email_is_provided():
     }
     http_response = sut.handle(http_request)
     assert http_response["statusCode"] == 400
-    assert http_response["body"].args == Exception("Missing param: email").args
+    assert http_response["body"].args == MissingParamError("email").args
