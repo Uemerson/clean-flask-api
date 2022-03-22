e00e1aa356584201131c93e7a3e1ffb507b7bb15
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-03-21 21:01:39 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- tests/presentation/controllers/test_signup_controller.py
+++ tests/presentation/controllers/test_signup_controller.py
@@ -1,9 +1,8 @@
 from src.presentation.controllers.signup_controller import SignUpController
 
 
-def test_return_400_if_no_name_is_provided():
+def test_should_return_400_if_no_name_is_provided():
     sut = SignUpController()
-
     http_request = {
         "body": {
             "email": "any_email@mail.com",
@@ -11,10 +10,20 @@ def test_return_400_if_no_name_is_provided():
             "password_confirmation": "any_password",
         }
     }
-
     http_response = sut.handle(http_request)
-
     assert http_response["statusCode"] == 400
     assert http_response["body"].args == Exception("Missing param: name").args
 
-    print("should return 400 if no name is provided")
+
+def test_should_return_400_if_no_email_is_provided():
+    sut = SignUpController()
+    http_request = {
+        "body": {
+            "name": "any_name",
+            "password": "any_password",
+            "password_confirmation": "any_password",
+        }
+    }
+    http_response = sut.handle(http_request)
+    assert http_response["statusCode"] == 400
+    assert http_response["body"].args == Exception("Missing param: email").args
