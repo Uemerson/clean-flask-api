38867d7163d7685efa0c9bbd8d123d708e8e7a9a
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-03-25 20:10:57 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup_controller.py
+++ src/presentation/controllers/signup_controller.py
@@ -1,5 +1,6 @@
 from src.presentation.errors.missing_param_error import MissingParamError
 from src.presentation.errors.invalid_param_error import InvalidParamError
+from src.presentation.errors.server_error import ServerError
 from src.presentation.helpers.http_helper import bad_request
 
 
@@ -8,10 +9,13 @@ class SignUpController:
         self._email_validator = email_validator
 
     def handle(self, http_request):
-        required_fields = ["name", "email", "password", "password_confirmation"]
-        for field in required_fields:
-            if http_request["body"].get(field) == None:
-                return bad_request(MissingParamError(field))
-        is_valid = self._email_validator.is_valid(http_request["body"]["email"])
-        if not is_valid:
-            return bad_request(InvalidParamError("email"))
+        try:
+            required_fields = ["name", "email", "password", "password_confirmation"]
+            for field in required_fields:
+                if http_request["body"].get(field) == None:
+                    return bad_request(MissingParamError(field))
+            is_valid = self._email_validator.is_valid(http_request["body"]["email"])
+            if not is_valid:
+                return bad_request(InvalidParamError("email"))
+        except:
+            return {"statusCode": 500, "body": ServerError()}
