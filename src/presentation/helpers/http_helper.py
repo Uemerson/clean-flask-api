8a97a64ee4af3280981fd110ea4afe7a035afa28
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-03-29 01:47:28 -0300

    refactor: make helpers conforms to a protocol

--- src/presentation/helpers/http_helper.py
+++ src/presentation/helpers/http_helper.py
@@ -1,9 +1,10 @@
+from src.presentation.protocols.http import HttpResponse
 from src.presentation.errors.server_error import ServerError
 
 
-def bad_request(error):
+def bad_request(error: Exception) -> HttpResponse:
     return {"statusCode": 400, "body": error}
 
 
-def server_error():
+def server_error() -> HttpResponse:
     return {"statusCode": 500, "body": ServerError()}
