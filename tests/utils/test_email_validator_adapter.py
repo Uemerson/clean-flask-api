1e306fa6eccebaafcc2b26d3f0e1da55d9ebcb68
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-04-04 22:14:25 -0300

    refactor: move sut creation to a factory helper method

--- tests/utils/test_email_validator_adapter.py
+++ tests/utils/test_email_validator_adapter.py
@@ -2,21 +2,25 @@ from src.utils.email_validator import EmailValidatorAdapter
 import validators
 
 
+def make_sut() -> EmailValidatorAdapter:
+    return EmailValidatorAdapter()
+
+
 def test_should_return_false_if_validator_returns_false(mocker):
     mocker.patch.object(validators, "email", return_value=False)
-    sut = EmailValidatorAdapter()
+    sut = make_sut()
     is_valid = sut.is_valid("invalid_email@mail.com")
     assert is_valid is False
 
 
 def test_should_return_true_if_validator_returns_true():
-    sut = EmailValidatorAdapter()
+    sut = make_sut()
     is_valid = sut.is_valid("valid_email@mail.com")
     assert is_valid is True
 
 
 def test_call_validator_with_correct_email(mocker):
-    sut = EmailValidatorAdapter()
+    sut = make_sut()
     spy = mocker.spy(validators, "email")
     sut.is_valid("any_email@mail.com")
     spy.assert_called_once_with("any_email@mail.com")
