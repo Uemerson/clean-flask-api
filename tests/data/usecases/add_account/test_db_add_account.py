2b1f5a77c7c64f193c8726ccbcbcff15eedb0796
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-04-05 21:34:10 -0300

    refactor: move sut creation to a factory helper method

--- tests/data/usecases/add_account/test_db_add_account.py
+++ tests/data/usecases/add_account/test_db_add_account.py
@@ -1,13 +1,20 @@
 from src.data.usecases.add_account.db_add_account import DbAddAccount
+from src.data.protocols.encrypter import Encrypter
+from typing import Tuple
 
 
-def test_should_call_encrypter_with_correct_password(mocker):
-    class EncrypterStub:
+def make_sut() -> Tuple[DbAddAccount, Encrypter]:
+    class EncrypterStub(Encrypter):
         def encrypt(self, value: str) -> str:
             return "hashed_password"
 
     encrypter_stub = EncrypterStub()
     sut = DbAddAccount(encrypter_stub)
+    return sut, encrypter_stub
+
+
+def test_should_call_encrypter_with_correct_password(mocker):
+    sut, encrypter_stub = make_sut()
     spy = mocker.spy(encrypter_stub, "encrypt")
     account_data = {
         "name": "valid_name",
