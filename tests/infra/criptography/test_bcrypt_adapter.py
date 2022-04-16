3db9a6e5b584e42ec00f770419cb925ff895c632
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-04-16 19:18:53 -0300

    refactor: move sut creation to a factory helper method

--- tests/infra/criptography/test_bcrypt_adapter.py
+++ tests/infra/criptography/test_bcrypt_adapter.py
@@ -2,17 +2,20 @@ import bcrypt
 from src.infra.criptography.bcrypt_adapter import BcryptAdapter
 
 
-def test_should_call_bcrypt_with_correct_values(mocker):
+def make_sut():
     salt = bcrypt.gensalt(12)
-    sut = BcryptAdapter(salt)
+    return BcryptAdapter(salt), salt
+
+
+def test_should_call_bcrypt_with_correct_values(mocker):
+    sut, salt = make_sut()
     spy = mocker.spy(bcrypt, "hashpw")
     sut.encrypt("any_value")
     spy.assert_called_once_with("any_value".encode(), salt)
 
 
 def test_should_return_a_hash_os_success(mocker):
-    salt = bcrypt.gensalt(12)
-    sut = BcryptAdapter(salt)
+    sut, _ = make_sut()
     mocker.patch.object(bcrypt, "hashpw", return_value="hash".encode())
     hash = sut.encrypt("any_value")
     assert hash == "hash"
