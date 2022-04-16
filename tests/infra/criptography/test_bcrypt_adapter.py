d0a41ee3494f27d52358708ec8ea92259783b0e2
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-04-16 19:15:40 -0300

    test: ensure BcrypterAdapter return a hash on success

--- tests/infra/criptography/test_bcrypt_adapter.py
+++ tests/infra/criptography/test_bcrypt_adapter.py
@@ -8,3 +8,11 @@ def test_should_call_bcrypt_with_correct_values(mocker):
     spy = mocker.spy(bcrypt, "hashpw")
     sut.encrypt("any_value")
     spy.assert_called_once_with("any_value".encode(), salt)
+
+
+def test_should_return_a_hash_os_success(mocker):
+    salt = bcrypt.gensalt(12)
+    sut = BcryptAdapter(salt)
+    mocker.patch.object(bcrypt, "hashpw", return_value="hash".encode())
+    hash = sut.encrypt("any_value")
+    assert hash == "hash"
